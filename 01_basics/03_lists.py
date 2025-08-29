43c7ac39611c6603e1069969e0b268f26c98bb2d
Prasad <aherprasad2811@gmail.com>
2025-08-29 14:47:01 +0000

    lists

---
+++ 01_basics/03_lists.py
@@ -0,0 +1,16 @@
+
+myList = [1,2,3,4,5,6,7,8,9,0]
+print(myList)
+
+myList.append(11)
+print(myList)
+
+myList.remove(3)
+print(myList)
+
+myList.sort()
+print(myList)
+
+myList.reverse()
+print(myList)
+
